39438ca26c58188b1a006b8be7dd57cd581aa712
Rotten-LKZ <Rotten_LKZ@163.com>
2022-05-11 23:13:23 +0800

    feat: master questions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref, watch } from 'vue';
 import data from './assets/data';
 import Highlight from './components/Highlight.vue';
 
@@ -8,17 +8,31 @@ interface Question {
   answer: string;
 }
 
+interface OneQuestion {
+  knowledge: number;
+  detail: number;
+}
+
 const question = ref<Question>({ question: "", answer: "" });
 const showAnswer = ref(false);
+const masteredQuestions = reactive<OneQuestion[]>(JSON.parse(window.localStorage.getItem('masteredQuestions') || '[]'));
+
+let nowQuestion: OneQuestion = { knowledge: -1, detail: -1 };
 
 newQuestion();
 
 function newQuestion() {
-  const r = data[Math.floor(Math.random() * data.length)];
+  const knowledgePlace = Math.floor(Math.random() * data.length);
+  const r = data[knowledgePlace];
   const elems = r.split('\n');
-  const q = elems[Math.floor(Math.random() * elems.length)].trim();
-  if (q === '' || q.endsWith('no-question') || q.endsWith('】')) {
-    newQuestion();
+  const detailPlace = Math.floor(Math.random() * elems.length);
+  const q = elems[detailPlace].trim();
+  if (q === '' || q.endsWith('no-question') || q.endsWith('】') || isMasteredQuestion(knowledgePlace, detailPlace)) {
+    if (masteredQuestions.length === calculateQuestions()) {
+      alert('你已熟悉所有题目！');
+    } else {
+      newQuestion();
+    }
     return;
   }
 
@@ -31,6 +45,7 @@ function newQuestion() {
       answer += `<br/>${elems[i]}`;
   }
   showAnswer.value = false;
+  nowQuestion = { knowledge: knowledgePlace, detail: detailPlace };
   question.value = { question: q.replace('no-newline', ''), answer: answer.substring(5) };
 }
 
@@ -46,6 +61,31 @@ function calculateQuestions() {
   }
   return questions;
 }
+
+function masterQuestion() {
+  if (masteredQuestions.length === calculateQuestions()) {
+    alert('你已熟悉所有题目！');
+  } else {
+    masteredQuestions.push(nowQuestion);
+    newQuestion();
+  }
+}
+
+function isMasteredQuestion(knowledgePlace: number, detailPlace: number) {
+  for (const masteredQuestion of masteredQuestions) {
+    if (masteredQuestion.knowledge === knowledgePlace && masteredQuestion.detail === detailPlace)
+      return true;
+  }
+  return false;
+}
+
+function restart() {
+  masteredQuestions.length = 0;
+}
+
+watch(masteredQuestions, value => {
+  window.localStorage.setItem('masteredQuestions', JSON.stringify(value));
+});
 </script>
 
 <template>
@@ -53,12 +93,14 @@ function calculateQuestions() {
     <p>{{ question.question }}</p>
     <button @click="showAnswer = !showAnswer" v-text="showAnswer ? '隐藏答案' : '揭晓答案'"></button>
     <button @click="newQuestion">换一道题</button>
+    <button @click="masterQuestion">非常熟悉</button>
+    <button @click="restart">重新来过</button>
     <!-- <p v-if="showAnswer" v-html="question.answer"></p> -->
     <Highlight :html="question.answer" :high-text="question.question" v-if="showAnswer" />
   </main>
 
   <footer>
-    共 {{ calculateQuestions() }} 道题
+    共 {{ calculateQuestions() }} 道题{{ masteredQuestions.length === 0 ? `` : `，已熟悉 ${masteredQuestions.length} 道题` }}
   </footer>
 </template>
 
