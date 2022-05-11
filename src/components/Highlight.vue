f6e8616fbc83824ac463d6a499fadf45be3ffdba
Rotten-LKZ <Rotten_LKZ@163.com>
2022-05-11 23:17:30 +0800

    refactor: highlight (support custom color)

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -8,10 +8,12 @@
   const props = defineProps<{
     html: string;
     highText: string;
+    color?: string;
   }>()
 
   const html = computed(() => {
-    return props.html.replace(RegExp(props.highText, 'g'), `<span class="highlight">${props.highText}</span>`);
+    return props.html.replace(RegExp(props.highText, 'g'), 
+    `<span class="highlight" ${props.color === undefined ? `` : `style="background-color:${props.color};"`}>${props.highText}</span>`);
   })
 </script>
 
